e5627b7d758ea5e4a53eef313e58864bd3660ea0
Miško Hevery <misko@hevery.com>
2023-05-22 09:14:08 -0700

    added prisma

--- src/routes/[org]/[repo]/index.tsx
+++ src/routes/[org]/[repo]/index.tsx
@@ -1,14 +1,22 @@
 import { component$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
+import {
+  Form,
+  routeAction$,
+  routeLoader$,
+  z,
+  zod$,
+} from "@builder.io/qwik-city";
+import { PrismaClient } from "@prisma/client";
 import { Octokit } from "octokit";
 
-export const useRepository = routeLoader$(async () => {
-  const owner = "BuilderIO";
-  const repo = "qwik";
+const octokit = new Octokit({
+  auth: import.meta.env.VITE_GITHUB_ACCESS_TOKEN,
+});
+const prisma = new PrismaClient();
 
-  const octokit = new Octokit({
-    auth: import.meta.env.VITE_GITHUB_ACCESS_TOKEN,
-  });
+export const useRepository = routeLoader$(async ({ params }) => {
+  const owner = params.org;
+  const repo = params.repo;
 
   const repository = await octokit.request("GET /repos/{owner}/{repo}", {
     owner,
@@ -19,12 +27,58 @@ export const useRepository = routeLoader$(async () => {
   return repository;
 });
 
+export const useIsFavorite = routeLoader$(async ({ params, sharedMap }) => {
+  const email = sharedMap.get("session")?.user?.email as string | undefined;
+  const org = params.org;
+  const repo = params.repo;
+  if (email) {
+    const users = await prisma.favorite.findMany({
+      where: { org, repo, email },
+    });
+    console.log("IS FAVORITE", users.length > 0);
+    return users.length > 0;
+  }
+  return false;
+});
+
+export const useSetFavoriteAction = routeAction$(
+  async ({ favorite }, { params, sharedMap }) => {
+    const email = sharedMap.get("session")?.user?.email as string | undefined;
+    const org = params.org;
+    const repo = params.repo;
+    console.log("SET FAVORITE", favorite, email, org, repo);
+    if (email) {
+      await prisma.favorite.deleteMany({
+        where: { org, repo, email },
+      });
+    }
+    if (favorite && email) {
+      await prisma.favorite.create({
+        data: { org, repo, email },
+      });
+    }
+  },
+  zod$({ favorite: z.coerce.boolean() })
+);
+
 export default component$(() => {
   const repository = useRepository();
+  const isFavorite = useIsFavorite();
+  const setFavoriteAction = useSetFavoriteAction();
   return (
     <div>
       <div>
         <b>Repo:</b> {repository.value.data.name}
+        <Form action={setFavoriteAction}>
+          <input
+            type="hidden"
+            name="favorite"
+            value={isFavorite.value ? "" : "true"}
+          />
+          <button style={{ color: isFavorite.value ? "red" : "gray" }}>
+            {isFavorite.value ? "★" : "☆"}
+          </button>
+        </Form>
       </div>
       <div>
         <b>Description:</b> {repository.value.data.description}
