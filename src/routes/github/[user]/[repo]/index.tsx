a2fb56eb9c340442d678ff9fd58da4578f088ceb
Miško Hevery <misko@hevery.com>
2023-05-22 21:38:06 -0700

    Lesson 4

--- src/routes/github/[user]/[repo]/index.tsx
+++ src/routes/github/[user]/[repo]/index.tsx
@@ -1,6 +1,14 @@
 import { component$ } from "@builder.io/qwik";
-import { useLocation, routeLoader$ } from "@builder.io/qwik-city";
+import {
+  Form,
+  routeAction$,
+  routeLoader$,
+  useLocation,
+} from "@builder.io/qwik-city";
+
+import { z, zod$ } from "@builder.io/qwik-city";
 import type { paths } from "@octokit/openapi-types";
+import { createServerClient } from "supabase-auth-helpers-qwik";
 
 type OrgRepoResponse =
   paths["/repos/{owner}/{repo}"]["get"]["responses"]["200"]["content"]["application/json"];
@@ -20,8 +28,70 @@ export const useRepository = routeLoader$(async ({ params, env }) => {
   return repository;
 });
 
+export const useIsFavorite = routeLoader$(async (requestEv) => {
+  const email = requestEv.sharedMap.get("session")?.user?.email as
+    | string
+    | undefined;
+  const user = requestEv.params.user;
+  const repo = requestEv.params.repo;
+  if (email) {
+    const supabaseClient = createServerClient(
+      requestEv.env.get("PRIVATE_SUPABASE_URL")!,
+      requestEv.env.get("PRIVATE_SUPABASE_ANON_KEY")!,
+      requestEv
+    );
+    const { data: favorite, error } = await supabaseClient
+      .from("favorite")
+      .select("*")
+      .eq("email", email)
+      .eq("user", user)
+      .eq("repo", repo);
+    if (error) {
+      console.log("ERROR", error);
+      throw error;
+    }
+    console.log("IS FAVORITE", favorite?.length, email, user, repo);
+    return (favorite?.length || 0) > 0;
+  }
+  return false;
+});
+
+export const useSetFavoriteAction = routeAction$(
+  async ({ favorite }, requestEv) => {
+    const email = requestEv.sharedMap.get("session")?.user?.email as
+      | string
+      | undefined;
+    const user = requestEv.params.user;
+    const repo = requestEv.params.repo;
+    console.log("SET FAVORITE", favorite, email, user, repo);
+    const supabaseClient = createServerClient(
+      requestEv.env.get("PRIVATE_SUPABASE_URL")!,
+      requestEv.env.get("PRIVATE_SUPABASE_ANON_KEY")!,
+      requestEv
+    );
+    if (email) {
+      await supabaseClient
+        .from("favorite")
+        .delete()
+        .eq("email", email)
+        .eq("user", user)
+        .eq("repo", repo);
+    }
+    if (favorite && email) {
+      await supabaseClient.from("favorite").upsert({
+        email,
+        user,
+        repo,
+      });
+    }
+  },
+  zod$({ favorite: z.coerce.boolean() })
+);
+
 export default component$(() => {
   const repository = useRepository();
+  const isFavorite = useIsFavorite();
+  const setFavoriteAction = useSetFavoriteAction();
   const location = useLocation();
   return (
     <div>
@@ -32,6 +102,16 @@ export default component$(() => {
       </h1>
       <div>
         <b>Repo:</b> {repository.value.name}
+        <Form action={setFavoriteAction}>
+          <input
+            type="hidden"
+            name="favorite"
+            value={isFavorite.value ? "" : "true"}
+          />
+          <button style={{ color: isFavorite.value ? "red" : "gray" }}>
+            {isFavorite.value ? "★" : "☆"}
+          </button>
+        </Form>
       </div>
       <div>
         <b>Description:</b> {repository.value.description}
