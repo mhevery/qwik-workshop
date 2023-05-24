5bd0ca62051df3e0a60751310b2fe6bcb57808a6
gioboa <giorgiob.boa@gmail.com>
2023-05-24 23:21:55 +0200

    added the header style

--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -10,7 +10,7 @@ export const User = component$<Props>(({ user }) => {
 	return (
 		<div class='flex container mx-auto p-4 mb-4 bg-slate-600 w-[350px] rounded-2xl overflow-hidden'>
 			<img
-				class='rounded-xl'
+				class='rounded-xl min-w-[150px] max-w-[150px]'
 				height='150'
 				width='150'
 				src={user.avatar_url}
@@ -21,7 +21,7 @@ export const User = component$<Props>(({ user }) => {
 					{user.login}
 				</div>
 				<a
-					class='flex items-center text-white text-md font-semibold bg-teal-500 py-2 px-4 rounded-lg hover:bg-teal-400'
+					class='flex items-center text-white text-md font-semibold bg-cyan-500 py-2 px-4 rounded-lg hover:bg-cyan-400'
 					href={`/design-github/${user.login}/`}
 				>
 					Detail
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,43 +1,53 @@
-import { component$, Slot, useStylesScoped$ } from "@builder.io/qwik";
-import { useAuthSession, useAuthSignout, useAuthSignin } from "./plugin@auth";
-import { Form, useLocation } from "@builder.io/qwik-city";
-import CSS from "./layout.css?inline";
+import { component$, Slot, useStylesScoped$ } from '@builder.io/qwik';
+import { useAuthSession, useAuthSignout, useAuthSignin } from './plugin@auth';
+import { Form, useLocation } from '@builder.io/qwik-city';
+import CSS from './layout.css?inline';
 
 export default component$(() => {
-  useStylesScoped$(CSS);
-  const session = useAuthSession();
-  const authSignin = useAuthSignin();
-  const authSignout = useAuthSignout();
-  const location = useLocation();
-  const user = session.value?.user;
-  return (
-    <>
-      <header>
-        <div>
-          {user ? (
-            <div>
-              <Form action={authSignout}>
-                {user.image && <img src={user.image} width={30} height={30} />}
-                <span>
-                  {user.name}({user.email})
-                </span>
-                <button>Sign Out</button>
-              </Form>
-            </div>
-          ) : (
-            <Form action={authSignin}>
-              <input type="hidden" name="providerId" value="github" />
-              <input
-                type="hidden"
-                name="options.callbackUrl"
-                value={location.url.toString()}
-              />
-              <button>Sign In</button>
-            </Form>
-          )}
-        </div>
-      </header>
-      <Slot />
-    </>
-  );
+	useStylesScoped$(CSS);
+	const session = useAuthSession();
+	const authSignin = useAuthSignin();
+	const authSignout = useAuthSignout();
+	const location = useLocation();
+	const user = session.value?.user;
+	return (
+		<>
+			<header
+				class='flex items-center bg-slate-600 py-4'
+				style='justify-content: space-between'
+			>
+				<h2 class='text-white font-bold text-2xl pl-4'>Qwik Workshop</h2>
+				<div class='flex items-center'>
+					{user ? (
+						<Form action={authSignout}>
+							<div class='flex items-center'>
+								{user.image && (
+									<img src={user.image} class='mr-2' width={60} height={60} />
+								)}
+								<div class='text-white mr-4'>
+									{user.name}({user.email})
+								</div>
+								<button class='bg-cyan-500 text-white p-2 rounded mr-4 hover:bg-cyan-400'>
+									Sign Out
+								</button>
+							</div>
+						</Form>
+					) : (
+						<Form action={authSignin}>
+							<input type='hidden' name='providerId' value='github' />
+							<input
+								type='hidden'
+								name='options.callbackUrl'
+								value={location.url.toString()}
+							/>
+							<button class='bg-cyan-500 text-white p-2 rounded mr-4 hover:bg-cyan-400'>
+								Sign in
+							</button>
+						</Form>
+					)}
+				</div>
+			</header>
+			<Slot />
+		</>
+	);
 });
